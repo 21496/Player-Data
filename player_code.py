4c787709d41b213f163f2131e7089f490519c6f5
21496 <22425@burnside.school.nz>
2024-05-23 09:34:54 +1200

    Added Units

--- player_code.py
+++ player_code.py
@@ -4,6 +4,7 @@ import sqlite3
 
 #constants and variables
 DATABASE = "player.db"
+PASSWORD1 = "pbkdf2:sha256:600000$ZI1UeAvvyxlnqov2$8a8d9f8345109ad68e25766bb4bc7c7f52030e11643e818405bf390b1ee12e60"
 
 #functions
 def print_all_data():
@@ -13,9 +14,9 @@ def print_all_data():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all the results
-    print("ID  Name                Top_Speed  Distance  Average")
+    print("ID  Name                Top_Speed     Distance      Average")
     for data in results:
-        print(f"{data[0]:<4}{data[1]:<20}{data[2]:<11}{data[3]:<10}{data[4]:<6}")
+        print(f"{data[0]:<4}{data[1]:<20}{data[2]}{" km/hr":<12}{data[3]}{" km":<10}{data[4]}{" km/hr":<6}")
     #loop finshed here
     db.close()
 
@@ -26,9 +27,9 @@ def print_top_speed():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all the results
-    print("ID  Name                Top_Speed  Distance  Average")
+    print("ID  Name                Top_Speed     Distance      Average")
     for data in results:
-        print(f"{data[0]:<4}{data[1]:<20}{data[2]:<11}{data[3]:<10}{data[4]:<6}")
+        print(f"{data[0]:<4}{data[1]:<20}{data[2]}{" km/hr":<12}{data[3]}{" km":<10}{data[4]}{" km/hr":<6}")
     #loop finshed here
     db.close()
 
@@ -39,9 +40,9 @@ def print_total_distance():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all the results
-    print("ID  Name                Top_Speed  Distance  Average")
+    print("ID  Name                Top_Speed     Distance      Average")
     for data in results:
-        print(f"{data[0]:<4}{data[1]:<20}{data[2]:<11}{data[3]:<10}{data[4]:<6}")
+        print(f"{data[0]:<4}{data[1]:<20}{data[2]}{" km/hr":<12}{data[3]}{" km":<10}{data[4]}{" km/hr":<6}")
     #loop finshed here
     db.close()
 
@@ -52,11 +53,13 @@ def print_average_speed():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all the results
-    print("ID  Name                Top_Speed  Distance  Average")
+    print("ID  Name                Top_Speed     Distance      Average")
     for data in results:
-        print(f"{data[0]:<4}{data[1]:<20}{data[2]:<11}{data[3]:<10}{data[4]:<6}")
+        print(f"{data[0]:<4}{data[1]:<20}{data[2]}{" km/hr":<12}{data[3]}{" km":<10}{data[4]}{" km/hr":<6}")
     #loop finshed here
     db.close()
+
+
 while True:
     user_input = input(
 """
@@ -68,16 +71,15 @@ What would you like to do?
 5. Exit
 """
     )
-    try:
-        if user_input == "1":
-            print_all_data()
-        elif user_input == "2":
-            print_top_speed()
-        elif user_input == "3":
-            print_total_distance()
-        elif user_input == "4":
-            print_average_speed()
-        elif user_input == "5":
-            break
-    except ValueError:
+    if user_input == "1":
+        print_all_data()
+    elif user_input == "2":
+        print_top_speed()
+    elif user_input == "3":
+        print_total_distance()
+    elif user_input == "4":
+        print_average_speed()
+    elif user_input == "5":
+        break
+    else:
         print("That was not an option!\n")
